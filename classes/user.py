3bc5f1082e9c2870b364b65bbb3dbacce65b3421
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-10-22 10:56:40 +0200

    Update user.py

--- classes/user.py
+++ classes/user.py
@@ -1,13 +1,12 @@
 class User:
-  def __init__(self, username, id, location, char_ids, char_slots):
+  def __init__(self, username, id, char_ids, char_slots):
     self.id = id
     self.username = username # str
-    self.location = location # list(ints)
     self.char_ids = char_ids# list(ints)
     self.char_slots = char_slots
 
   def to_dict(self):
-    return {"id": self.id, "user": self.username, "location": self.location, "char_ids": self.char_ids, "char_slots": self.char_slots}
+    return {"id": self.id, "user": self.username, "char_ids": self.char_ids, "char_slots": self.char_slots}
   
   def __repr__(self):
     return f"User(id={self.id}, username={self.username})"
